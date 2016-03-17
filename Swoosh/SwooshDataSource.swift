ce01aed0576f9b8109ffcde610756b163086f4a5
Ariunjargal <abatjarg@icloud.com>
2016-03-16 23:45:19 -0400

    Initial commit. Setup the initial collection view.

---
+++ Swoosh/SwooshDataSource.swift
@@ -0,0 +1,24 @@
+//
+//  SwooshDataSource.swift
+//  Swoosh
+//
+//  Created by Ariunjargal on 3/16/16.
+//  Copyright © 2016 Ariunjargal. All rights reserved.
+//
+
+import UIKit
+
+class SwooshDataSource: NSObject, UICollectionViewDataSource {
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let identifier = "Cell"
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(identifier, forIndexPath: indexPath)
+        
+        return cell
+    }
+    
+}
