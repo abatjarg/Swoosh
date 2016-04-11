aadcd0032479c3dafae90f9ceaf23cdeb89c1408
Ariunjargal <abatjarg@icloud.com>
2016-04-10 22:42:51 -0400

    Added in collection view to user page

--- Swoosh/SwooshUserDetailViewController.swift
+++ Swoosh/SwooshUserDetailViewController.swift
@@ -8,13 +8,15 @@
 
 import UIKit
 
-class SwooshUserDetailViewController: UIViewController {
+class SwooshUserDetailViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
     var user: User! {
         didSet {
             navigationItem.title = user.name
         }
     }
+    
+    @IBOutlet var collectionView: UICollectionView!
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
@@ -25,10 +27,24 @@ class SwooshUserDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
         self.imageView.image = user.image
         self.nameLabel.text = user.name
         self.usernameLabel.text = user.username
         self.bioLabel.text = user.bio
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let identifier = "Cell"
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(identifier, forIndexPath: indexPath) as! UICollectionViewCell
+        
+        return cell
     }
     
 }
